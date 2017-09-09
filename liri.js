221b78e9e932c833d0ba02673525aa0d77f8d790
John Desrosiers <johndrunner@yahoo.com>
2017-09-09 15:09:06 -0700

    setup

---
+++ liri.js
@@ -0,0 +1,18 @@
+var cmd = process.argv[2];
+
+switch ( cmd ) {
+	case "my-tweets":
+		break;
+
+	case "spotify-this-song":
+		break;
+
+	case "movie-this":
+		break;
+
+	case "do-what-it-says":
+		break;
+		
+	default:
+		console.log("Invalid commeand:", cmd);
+}
